103cfa7290c8bf1594673a2ff2583e6f589a01e7
Da Li <daniel.lee.bit@gmail.com>
2015-08-14 02:36:37 -0500

    + Add regression model

--- chapter2/SimpleLinearRegression.py
+++ chapter2/SimpleLinearRegression.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 x = [ [6], [8], [10], [14], [18] ]
 y = [ [7], [9], [13], [17.5], [18] ]
+
+model = LinearRegression()
+model.fit(x,y)
+print 'A 12" pizza should cost: $%.2f' % model.predict([12])[0]
 
 plt.figure()
 plt.title('Pizza price plotted against diameter')
@@ -9,6 +14,5 @@ plt.xlabel('Diameter in inches')
 plt.ylabel('Price in dollars')
 plt.plot(x, y, 'k.')
 plt.axis([0, 25, 0, 25])
-
 plt.grid(True)
 plt.show()
